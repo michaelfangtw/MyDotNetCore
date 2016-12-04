09c27f1e37014d1f63f62ea8bfc2d74f2bf09474
Michael Fang <yingchih.fang@gmail.com>
2016-12-05 06:29:44 +0800

    dotnet core

---
+++ README.md
@@ -0,0 +1,64 @@
+
+MyWPFDemo
+-----
+This is a DotNet Core Demo<br>
+
+Installation
+------------
+```
+git clone MySingletonMultiDatabase https://github.com/michaelfangtw/MyDotNetCore.git
+```
+
+Git Notes
+------------
+```
+d:
+cd D:\git\DotNetCore
+git init
+git add .
+git status
+git commit -m "first commit"
+rem MyWPFDemo
+git remote add origin https://github.com/michaelfangtw/MyDotNetCore.git
+rem origin =remote repos
+rem master =local repos
+git push -u origin master
+```
+
+Usage
+------------
+Source /src/*.*
+
+Issues
+-------
+Found a bug? Create an issue on GitHub.
+
+
+For transparency and insight into the release cycle, releases will be numbered with the follow format:
+
+`<major>.<minor>.<patch>`
+
+And constructed with the following guidelines:
+
+* Breaking backwards compatibility bumps the major (無法向前相容)
+* New additions without breaking backwards compatibility bumps the minor (可向前相容的功能新增)
+* Bug fixes and misc changes bump the patch (修正Bug)
+
+For more information on semantic versioning, please visit http://semver.org/.
+
+License
+-------
+
+Copyright (c) 2016 [Michael Fang](http://funtech.tw)  
+Licensed under the MIT License.
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            byte byteValue = 16;
+            short shortValue = -1024;
+            int intValue = -1034000;
+            long longValue = 1152921504606846976;
+            ulong ulongValue = UInt64.MaxValue;
+
+            decimal decimalValue;
+
+            decimalValue = byteValue;
+            Console.WriteLine("After assigning a {0} value, the Decimal value is {1}.", 
+                            byteValue.GetType().Name, decimalValue); 
+
+            decimalValue = shortValue;
+            Console.WriteLine("After assigning a {0} value, the Decimal value is {1}.", 
+                            shortValue.GetType().Name, decimalValue); 
+
+            decimalValue = intValue;
+            Console.WriteLine("After assigning a {0} value, the Decimal value is {1}.", 
+                            intValue.GetType().Name, decimalValue); 
+
+            decimalValue = longValue;
+            Console.WriteLine("After assigning a {0} value, the Decimal value is {1}.", 
+                            longValue.GetType().Name, decimalValue); 
+
+            decimalValue = ulongValue;
+            Console.WriteLine("After assigning a {0} value, the Decimal value is {1}.", 
+                            longValue.GetType().Name, decimalValue); 
+            // The example displays the following output:
+            //    After assigning a Byte value, the Decimal value is 16.
+            //    After assigning a Int16 value, the Decimal value is -1024.
+            //    After assigning a Int32 value, the Decimal value is -1034000.
+            //    After assigning a Int64 value, the Decimal value is 1152921504606846976.
+            //    After assigning a Int64 value, the Decimal value is 18446744073709551615.
+        }
+    }
+}
